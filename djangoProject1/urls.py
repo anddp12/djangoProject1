725e6ee690f18aa5c24668f14bb87f6a37e5fbc9
anddp12 <anddp12@gmail.com>
2023-02-23 02:00:06 +0200

    home application routing

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from djangoProject1 import views
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('home.urls')),
-    path('', views.index),
-    path('about', views.about)
+    path('', include('home.urls')),
+    path('about', include('home.urls')),
+    # path('', views.index),
+    # path('about', views.about)
 ]
--- home/views.py
+++ home/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>HOME</h1>")
+    # return HttpResponse("<h1>HOME</h1>")
+    return render(request, 'index.html')
 
 
 def about(request):
-    return HttpResponse("<h1>ABOUT</h1>")
+    # return HttpResponse("<h1>ABOUT</h1>")
+    return render(request, 'about.html')
